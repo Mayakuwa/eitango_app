7aeea67b082f6eceee7416710b34d001d0fea6f4
Maya <melokitty.1003@gmail.com>
2021-01-06 14:43:49 +0900

    add animation when true

--- lib/screen/eitango_test_screen.dart
+++ lib/screen/eitango_test_screen.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:eitango_app/brain/question_brain.dart';
 import 'package:eitango_app/widget/answer_tile.dart';
+import 'package:flare_flutter/flare_actor.dart';
 
 QuestionBrain questionBrain = QuestionBrain();
 
-enum Answers {
-  YES,
-  NO
-}
 
 class EitangoTestScreen extends StatefulWidget {
   static const routeName= './eitango_test_screen';
@@ -24,31 +21,46 @@ class _EitangoTestScreenState extends State<EitangoTestScreen> {
   }
 
   void checkedAnswer(String word) async {
-    createDialogOption(BuildContext context, Answers answer, String str) {
-      return new SimpleDialogOption(child: new Text(str),onPressed: (){Navigator.pop(context, answer);},);
-    }
-
     if(questionBrain.getWordToJapanese() == word) {
       // show true Snackbar
-      await showDialog<Answers>(
+      await showDialog(
           context: context,
-          builder: (BuildContext context) => new SimpleDialog(
-            title: new Text('SimpleDialog'),
-            children: <Widget>[
-              createDialogOption(context, Answers.YES, 'Yes'),
-              createDialogOption(context, Answers.NO, 'No')
-            ],
-          ),
-      ).then((value) {
-        switch(value) {
-          case Answers.YES:
-            break;
-          case Answers.NO:
-            Navigator.of(context).pop();
-            break;
-        }
-
-      });
+          builder: (_) {
+            return AlertDialog(
+              title: SizedBox(
+                height: 60,
+                child: FlareActor(
+                  'images/SuccessCheck.flr',
+                  alignment: Alignment.center,
+                  animation: 'Untitled',
+                  fit: BoxFit.contain
+                ),
+              ),
+              content: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text('テストメッセージ')
+                ],
+              ),
+              actions: <Widget>[
+                FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('ok'),
+                ),
+                FlatButton(
+                  onPressed: () {
+                    // go to dictionary
+                  },
+                  child: Text('go to dictionary'),
+                )
+              ],
+            );
+          }
+      );
+      // go to next question
       questionBrain.nextQuestion();
       print('true');
       // add score kipper
@@ -57,6 +69,7 @@ class _EitangoTestScreenState extends State<EitangoTestScreen> {
       });
     } else {
       // show false SnackBar
+      questionBrain.nextQuestion();
       print('false');
       setState(() {
         scoreKeeper.add(Icon(Icons.close, color: Colors.red));
