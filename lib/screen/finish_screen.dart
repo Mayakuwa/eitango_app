d038a752a53a1684b2929374349e216f424ff6c7
Maya <melokitty.1003@gmail.com>
2021-01-07 15:13:56 +0900

    add result screen to animation

--- lib/screen/finish_screen.dart
+++ lib/screen/finish_screen.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
 
 class FinishScreen extends StatelessWidget {
   static const routeName = './finish_screen';
   @override
   Widget build(BuildContext context) {
-    print('${ModalRoute.of(context).settings.arguments}個正解');
+    final correctAnswer = ModalRoute.of(context).settings.arguments;
+    print('$correctAnswer個正解');
     return Scaffold(
       appBar: AppBar(title: Text('result')),
-      body: Container(),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: FlareActor(
+              'images/Trofeo.flr',
+              alignment: Alignment.center,
+              animation: 'Untitled',
+              fit: BoxFit.contain,
+            ),
+          ),
+          SizedBox(height: 40),
+          Expanded(
+            child: Text(
+              '$correctAnswer個正解です！',
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30
+              ),
+            ),
+          )
+        ],
+      )
     );
   }
 }
